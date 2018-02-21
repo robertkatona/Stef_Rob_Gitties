11dc4db65dd0b55580eeb34ef9f7831a13e9e360
stefanrakic001 <stefan.rakic001@gmail.com>
2018-02-21 10:19:25 +0100

    intro

--- gittie.py
+++ gittie.py
@@ -1,15 +1,14 @@
 def introduce():
-    print('Hello i am Gitte')
+    print('Hello i am Judit')
 
 
-<<<<<<< HEAD
 def joke():
     print('You are a joke...')
-=======
+
+
 def add(a,b):
     p = a + b
     print(p)
 
 
 #add(1,3)
->>>>>>> aab5a42fdd13014e9ab421c8b2a356516cde1c67
